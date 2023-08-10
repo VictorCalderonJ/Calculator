1b803cf5adaf85b9a400c5d2a2c6183f56545834
victor calderon <calderon.j.victor@gmail.com>
2023-08-10 09:37:31 -0500

    stylingadded

---
+++ JS/calc.js
@@ -0,0 +1,9 @@
+
+document.getElementById('example');
+const example1 = document.getElementById('example');
+console.log(example1);
+
+document.getElementById('example2');
+const example2 = document.getElementById('example2');
+console.log(example2);
+
